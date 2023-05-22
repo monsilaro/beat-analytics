6de56ccbd70fb1a09564f9f8923817cd43194176
Simon Larouche <slarouche@progymedia.com>
2023-05-22 17:10:17 -0400

    fix build

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,7 @@ import './page.scss';
 import React from 'react';
 
 export default function Profile() {
+    const user = 'Simon';
     return (
         <div className='profile'>
             <h2>{`Welcome ${user}`}</h2>
